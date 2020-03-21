c0c2ddf728f0f17a8a9b20bbebcf9f52e8991294
EvgenPetrovich8 <evgenpetrovich8@gmail.com>
2020-03-21 22:23:24 +0200

    Lecture 4 edit

--- src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
@@ -25,26 +25,26 @@ public class BinaryToDecimalChecker {
 
 //        TODO implements result
         int result = 0;
-        String  data = String.valueOf(value);
-        char[] dataDiv = data.toCharArray();
-
-        int pow  =0;
-        int length = dataDiv.length;
-        int i =0;
-
-        while(true){
-            if(dataDiv[i] == 0){
-                break;
-            } else {
-                int temp = dataDiv[i]%10;
-                result += temp*Math.pow(2, pow);
-//                dataDiv[i] = dataDiv[i]/10.;
-
-//                dataDiv[i] =  Integer.valueOf(dataDiv[i].charAt(0));
- //               dataDiv[i]=  Character.digit(dataDiv.charAt(0), 10);
-                pow++;
-            }
-        }
+//        String  data = String.valueOf(value);
+//        char[] dataDiv = data.toCharArray();
+//
+//        int pow  =0;
+//        int length = dataDiv.length;
+//        int i =0;
+//
+//        while(true){
+//            if(dataDiv[i] == 0){
+//                break;
+//            } else {
+//                int temp = dataDiv[i]%10;
+//                result += temp*Math.pow(2, pow);
+////                dataDiv[i] = dataDiv[i]/10.;
+//
+////                dataDiv[i] =  Integer.valueOf(dataDiv[i].charAt(0));
+// //               dataDiv[i]=  Character.digit(dataDiv.charAt(0), 10);
+//                pow++;
+//            }
+//        }
 
         return result;
     }
