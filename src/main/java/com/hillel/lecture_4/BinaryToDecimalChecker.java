f29cebd5def0c6bcc97b6aadf18cdd43421eb39a
EvgenPetrovich8 <evgenpetrovich8@gmail.com>
2020-03-25 18:54:50 +0200

    Add 2 tasks to lecture4

--- src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
@@ -11,9 +11,34 @@ public class BinaryToDecimalChecker {
     public int fromDecimalToBinary(int value) {
 
 //        TODO implements result
-        int result = 0;
+
+        int binaryArray[];
+       // int resultArray[] = new int[10];
+
+//        String temp = Integer.toString(value);
+//        int[] newCnt = new int[temp.length()];
+//        for (int i = 0; i < temp.length(); i++)
+//        {
+//            newCnt[i] = temp.charAt(i) - '0';
+//        }
+
+        int count =0 ;
+
+        int result=0;
+
+        String binary = "";
+
+        while(value>0){
 
 
+            int newres = value % 2;
+            binary= newres+binary;
+            value = value/2;
+
+
+        }
+
+       result=Integer.parseInt(binary);
 
 
 
@@ -27,24 +52,18 @@ public class BinaryToDecimalChecker {
         int result = 0;
 //        String  data = String.valueOf(value);
 //        char[] dataDiv = data.toCharArray();
-//
-//        int pow  =0;
-//        int length = dataDiv.length;
-//        int i =0;
-//
-//        while(true){
-//            if(dataDiv[i] == 0){
-//                break;
-//            } else {
-//                int temp = dataDiv[i]%10;
-//                result += temp*Math.pow(2, pow);
-////                dataDiv[i] = dataDiv[i]/10.;
-//
-////                dataDiv[i] =  Integer.valueOf(dataDiv[i].charAt(0));
-// //               dataDiv[i]=  Character.digit(dataDiv.charAt(0), 10);
-//                pow++;
-//            }
-//        }
+       int powCoef  =1;
+       int temp = value;
+               while (temp>0) {
+            int tempDigit = temp % 10;
+            temp = temp/10;
+                   result += tempDigit*powCoef;
+
+                   powCoef=powCoef*2;
+
+
+        }
+
 
         return result;
     }
--- src/main/java/com/hillel/lecture_4/DecimalToHexadecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/DecimalToHexadecimalChecker.java
@@ -12,6 +12,14 @@ public class DecimalToHexadecimalChecker {
 
 //        TODO implements result
         String result = "";
+        char[] hexSet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F'};
+        while (value !=0) {
+            int temp = value % 16;
+            result=hexSet[temp]+result;
+            value = value/16;
+
+        }
 
         return result;
     }
@@ -21,6 +29,26 @@ public class DecimalToHexadecimalChecker {
 
 //        TODO implements result
         int result = 0;
+
+        int coefByPow=1;
+
+        int lengthVal =value.length();
+
+        for (int count=lengthVal-1; count>=0; count--)
+        {
+            if(value.charAt(count)>='0'&& value.charAt(count)<='9'){
+
+                result += (value.charAt(count)-48)*coefByPow;
+                coefByPow = coefByPow*16;
+            }
+
+            else if (value.charAt(count)>='A'&& value.charAt(count)<='F'){
+                result += (value.charAt(count)-55)*coefByPow;
+                coefByPow = coefByPow*16;
+            }
+        }
+
+
 
         return result;
     }
